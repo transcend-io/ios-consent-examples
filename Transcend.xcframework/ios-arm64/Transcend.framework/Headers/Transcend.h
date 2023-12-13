c922467d2f356803c62b04a510650d93c206c3b9
JonnavithulaGirish <girish@transcend.io>
2023-12-12 20:21:07 -0600

    Use Transcend Framework inSwift Storyboard app

---
+++ Transcend.xcframework/ios-arm64/Transcend.framework/Headers/Transcend.h
@@ -0,0 +1,18 @@
+//
+//  Transcend.h
+//  Transcend
+//
+//  Created by Girish Jonnavithula on 12/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Transcend.
+FOUNDATION_EXPORT double TranscendVersionNumber;
+
+//! Project version string for Transcend.
+FOUNDATION_EXPORT const unsigned char TranscendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transcend/PublicHeader.h>
+
+
